08aac6043f2de12028f2cf98abd4408d49a60fb5
Asad Faraz <asadfaraz44@gmail.com>
2021-04-02 11:09:14 +0500

    First Commit

---
+++ src/data.js
@@ -0,0 +1,9 @@
+// // Array of objects
+
+// export const data = [
+//   { id: 1, name: "Asad" },
+//   { id: 2, name: "Arsalan" },
+//   { id: 3, name: "Haya" },
+//   { id: 4, name: "Jahan" },
+//   { id: 5, name: "Ayesha" },
+// ];
