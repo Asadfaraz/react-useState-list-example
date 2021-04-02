68e89d6a2ad5acbbba3fc42d4542e1ec32b9375a
Asad Faraz <asadfaraz44@gmail.com>
2021-04-02 11:34:59 +0500

    Reading data and CSS added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import { data } from "./data";
 
 function App() {
-  return <section className="App"></section>;
+  // useSTate for data
+  const [people, setPeople] = React.useState(data);
+
+  return (
+    <section className="App">
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="persondiv">
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
-// // Array of objects
+// Array of objects
 
-// export const data = [
-//   { id: 1, name: "Asad" },
-//   { id: 2, name: "Arsalan" },
-//   { id: 3, name: "Haya" },
-//   { id: 4, name: "Jahan" },
-//   { id: 5, name: "Ayesha" },
-// ];
+export const data = [
+  { id: 1, name: "Asad" },
+  { id: 2, name: "Arsalan" },
+  { id: 3, name: "Haya" },
+  { id: 4, name: "Jahan" },
+  { id: 5, name: "Ayesha" },
+];
